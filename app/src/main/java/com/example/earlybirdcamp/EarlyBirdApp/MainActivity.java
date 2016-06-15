77a66caa5f171588217b9ac332cdcd6144b496a8
earlybirdcamp <tranc2606@gmail.com>
2016-06-14 22:23:42 -0700

    added my name

--- app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/MainActivity.java
+++ app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/MainActivity.java
@@ -17,6 +17,7 @@ import com.twitter.sdk.android.core.*;
 import io.fabric.sdk.android.Fabric;
 
 // @author  Tuan Anh Tran added
+//@ John Doe
 
 
 public class MainActivity extends AppCompatActivity {
