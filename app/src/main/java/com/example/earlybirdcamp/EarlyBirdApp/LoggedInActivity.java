e75b872fd70d04e832f564742fa78a85ccca3f31
Lorem-Toastem <plushie.panda@gmail.com>
2016-06-14 23:27:44 -0700

    added features that do nothing

---
+++ app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/LoggedInActivity.java
@@ -0,0 +1,55 @@
+package com.example.earlybirdcamp.EarlyBirdApp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+
+public class LoggedInActivity extends AppCompatActivity {
+    private static final String TAG = "LoggedInActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                //Log.d(TAG, "Add a new task");
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .setNeutralButton("meh",null)
+                        .create();
+                dialog.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
