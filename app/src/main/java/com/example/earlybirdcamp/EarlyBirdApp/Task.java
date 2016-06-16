0a6d98d48921fa0da6915c5219dd02e2826e72f8
edwinvillafane <ehv02014@mymail.pomona.edu>
2016-06-15 21:27:06 -0700

    Some additions to task

--- app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/Task.java
+++ app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/Task.java
@@ -4,14 +4,37 @@ package com.example.earlybirdcamp.EarlyBirdApp;
  * Created by earlybirdcamp on 6/14/16.
  */
 public class Task {
-    private String mTitle;
 
-    public Task(String title){
-        mTitle = title;
+    String title;
+    String desc;
+    int days;
+
+    public Task(){
+
     }
 
     public String getTitle(){
-        return mTitle;
+        return title;
     }
-    //>:C
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDesc(){
+        return desc;
+    }
+
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+
+    public int getDays(){
+        return days;
+    }
+
+    public void setDays(int days){
+        this.days = days;
+    }
+
 }
