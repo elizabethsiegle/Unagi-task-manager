f4658805aa46eb42da1b83369fa18037e76107e6
Lorem-Toastem <plushie.panda@gmail.com>
2016-06-15 22:12:46 -0700

    slightly less crying

--- app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/TaskActivity.java
+++ app/src/main/java/com/example/earlybirdcamp/EarlyBirdApp/TaskActivity.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import android.util.Log;
 
 public class TaskActivity extends AppCompatActivity{
     private String user;
@@ -33,7 +39,7 @@ public class TaskActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                final AlertDialog dialog = new AlertDialog.Builder(this)
+                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("Add a new task")
                         .setMessage("What do you want to do next?")
                         //.setView(taskEditText)
@@ -46,12 +52,33 @@ public class TaskActivity extends AppCompatActivity{
                                 Spinner spin = (Spinner) dialog.findViewById(R.id.priority_spinner);
                                 String[]  array_name = getResources().getStringArray(R.array.priority_array);
 
+                                /*
                                 ArrayAdapter<String> adapter_state = new ArrayAdapter<String>(TaskActivity.this, android.R.layout.simple_spinner_item, array_name);
                                 adapter_state.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                spin.setAdapter(adapter_state);
+                                spin.setAdapter(adapter_state);*/
 
-                                Toast.makeText(TaskActivity.this, "this is my Toast message!!! =)",
-                                        Toast.LENGTH_LONG).show();
+                                spin.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+                                    @Override
+                                    public void onItemSelected(AdapterView<?> parent, View selectedItemView,
+                                                               int pos, long id) {
+                                        Toast.makeText(parent.getContext(),
+                                                "On Item Select : \n" + parent.getItemAtPosition(pos).toString(),
+                                                Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onNothingSelected(AdapterView<?> arg0) {
+                                        //  Auto-generated method stub
+
+                                    }
+
+                                });
+                                int select = spin.getSelectedItemPosition();
+                                Toast.makeText(TaskActivity.this, "this is my Toast message!!! =) " + select, Toast.LENGTH_LONG).show();
+
+                                //String text = spin.getSelectedItem().toString();
+
                                 //String task = String.valueOf(taskEditText.getText());
                                 //Log.d(TAG, "Task to add: " + task);
                                 //This is where we log the task stuff added
